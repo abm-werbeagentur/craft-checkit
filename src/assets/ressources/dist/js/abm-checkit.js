614b2884d11c0be5a106ed66eaa27d11df22316c
Stemmer <stemmer@abm.at>
2024-04-24 11:03:48 +0200

    added lightswitch indicate for all sites

--- src/assets/ressources/dist/js/abm-checkit.js
+++ src/assets/ressources/dist/js/abm-checkit.js
@@ -15,6 +15,8 @@ $(function() {
 			}
 		});
 	}
+
+	CheckitSwitches.init();
 	
 	$("#abm-checkit-sidebar *[data-toggle='collapse'], #abm-checkit-overview *[data-toggle='collapse']").on("click",function() {
 		if($(this).parents("#abm-checkit-overview").length) {
@@ -26,3 +28,73 @@ $(function() {
 		}
 	});
 });
+
+CheckitSwitches = {
+	$allCheckitSwitches: null,
+	$enabledForAllSitesField: null,
+	$enabledForAllSitesSwitch: null,
+
+	init: function() {
+
+		this.$allCheckitSwitches = $("#abm-checkit-sidebar .field.lightswitch-field button.lightswitch");
+
+		if(this.$allCheckitSwitches.length<2) {
+			return;
+		}
+		
+		$firstSite = $("#abm-checkit-sidebar .field.lightswitch-field").eq(0);
+
+		this.$enabledForAllSitesField = Craft.ui
+		.createLightswitchField({
+			label: Craft.t('abm-checkit', 'Indicate for all sites')
+		})
+		.insertBefore($firstSite);
+
+		this.$enabledForAllSitesField.find('label').css('font-weight', 'bold');
+		this.$enabledForAllSitesSwitch = this.$enabledForAllSitesField
+			.find('.lightswitch')
+			.on("change", this._updateAllSiteStatuses.bind(this));
+
+		this.$allCheckitSwitches.on("change", this._updateAllSiteStatus.bind(this));
+
+		this._updateAllSiteStatus();
+	},
+
+	_updateAllSiteStatus: function() {
+		let allEnabled = true,
+			allDisabled = true;
+
+		this.$allCheckitSwitches.each(function() {
+			const enabled = $(this).hasClass("on");
+			if (enabled) {
+				allDisabled = false;
+			} else {
+				allEnabled = false;
+			}
+
+			if (!allEnabled && !allDisabled) {
+				return false;
+			}
+		});
+		if (allEnabled) {
+			this.$enabledForAllSitesSwitch.data('lightswitch').turnOn(true);
+
+		  } else if (allDisabled) {
+			this.$enabledForAllSitesSwitch.data('lightswitch').turnOff(true);
+
+		  } else {
+			this.$enabledForAllSitesSwitch.data('lightswitch').turnIndeterminate(true);
+		  }
+	},
+
+	_updateAllSiteStatuses: function() {
+		const enabled = this.$enabledForAllSitesSwitch.data('lightswitch').on;
+		this.$allCheckitSwitches.each(function () {
+			if (enabled) {
+				$(this).data('lightswitch').turnOn(true);
+			} else {
+				$(this).data('lightswitch').turnOff(true);
+			}
+		});
+	},
+};
